3853a46f647f9558a3fdb482a837665d3f41c9de
lord-of-death <tim.steinbach93@gmail.com>
2022-05-21 10:53:44 +0200

    Changed spaces to tabs, updated input for calling the python script, changed the basler method to a camera object with multiple methods

--- semantic_segmentation.py
+++ semantic_segmentation.py
@@ -5,7 +5,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     https://www.apache.org/licenses/LICENSE-2.0
+#		 https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,10 +21,9 @@ segmented pixels at the location specified by `output`:
 bash examples/install_requirements.sh semantic_segmentation.py
 
 python3 examples/semantic_segmentation.py \
-  --model test_data/deeplabv3_mnv2_pascal_quant_edgetpu.tflite \
-  --input test_data/bird.bmp \
-  --keep_aspect_ratio \
-  --output ${HOME}/segmentation_result.jpg
+	--model test_data/deeplabv3_mnv2_pascal_quant_edgetpu.tflite \
+	--keep_aspect_ratio \
+	--output ${HOME}/segmentation_result.jpg
 ```
 """
 
@@ -42,148 +41,155 @@ from pycoral.adapters import segment
 from pycoral.utils.edgetpu import make_interpreter
 
 import sys
+import copy
 
-def myPylonImage():
-  img = pylon.PylonImage()
-  tlf = pylon.TlFactory.GetInstance()
+class BaslerCam():
+	#inits a Basler camera object
+	def __init__(self):
+		self.img = pylon.PylonImage()
+		self.tlf = pylon.TlFactory.GetInstance()
+		self.cam = pylon.InstantCamera(self.tlf.CreateFirstDevice())
 
-  cam = pylon.InstantCamera(tlf.CreateFirstDevice())
-  cam.Open()
-  cam.StartGrabbing()
+	#grabs a single image, converts it and returns the result
+	def grabImage(self):
+		self.cam.Open()
+		self.cam.StartGrabbing()
 
-  # converting to opencv bgr format
-  converter = pylon.ImageFormatConverter()
-  converter.OutputPixelFormat = pylon.PixelType_BGR8packed
-  converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
+		with self.cam.RetrieveResult(2000) as result:
 
-  with cam.RetrieveResult(2000) as result:
+			# Calling AttachGrabResultBuffer creates another reference to the
+			# grab result buffer. This prevents the buffer's reuse for grabbing.
+			self.img.AttachGrabResultBuffer(result)
 
-    # Calling AttachGrabResultBuffer creates another reference to the
-    # grab result buffer. This prevents the buffer's reuse for grabbing.
-    img.AttachGrabResultBuffer(result)
+			self.img.Release()
 
-    imageConverted = converter.Convert(result)
-    imageArray = imageConverted.GetArray()
-    '''
-    filename = "saved_pypylon_img.png"
-    img.Save(pylon.ImageFileFormat_Png, filename)
-
-    imgToSave = Image.open("saved_pypylon_img.png")
-    imgToSave.save("saved_pypylon_img.bmp")
-    '''
+			return self.convertImage(result)
 
 
-    img.Release()
+	# converts the picture of the camera to opencv bgr format and returns it
+	def convertImage(self, result):
+		converter = pylon.ImageFormatConverter()
+		converter.OutputPixelFormat = pylon.PixelType_BGR8packed
+		converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
 
-    return imageArray
+		print(result)
+		imageConverted = converter.Convert(result)
+		imageArray = imageConverted.GetArray()
+
+		'''
+		filename = "saved_pypylon_img.png"
+		img.Save(pylon.ImageFileFormat_Png, filename)
+
+		imgToSave = Image.open("saved_pypylon_img.png")
+		imgToSave.save("saved_pypylon_img.bmp")
+		'''
+		return imageArray
 
 def create_pascal_label_colormap():
-  """Creates a label colormap used in PASCAL VOC segmentation benchmark.
+	"""Creates a label colormap used in PASCAL VOC segmentation benchmark.
 
-  Returns:
-    A Colormap for visualizing segmentation results.
-  """
-  colormap = np.zeros((256, 3), dtype=int)
-  indices = np.arange(256, dtype=int)
+	Returns:
+		A Colormap for visualizing segmentation results.
+	"""
+	colormap = np.zeros((256, 3), dtype=int)
+	indices = np.arange(256, dtype=int)
 
-  for shift in reversed(range(8)):
-    for channel in range(3):
-      colormap[:, channel] |= ((indices >> channel) & 1) << shift
-    indices >>= 3
+	for shift in reversed(range(8)):
+		for channel in range(3):
+			colormap[:, channel] |= ((indices >> channel) & 1) << shift
+		indices >>= 3
 
-  return colormap
+	return colormap
 
 
 def label_to_color_image(label):
-  """Adds color defined by the dataset colormap to the label.
+	"""Adds color defined by the dataset colormap to the label.
 
-  Args:
-    label: A 2D array with integer type, storing the segmentation label.
+	Args:
+		label: A 2D array with integer type, storing the segmentation label.
 
-  Returns:
-    result: A 2D array with floating type. The element of the array
-      is the color indexed by the corresponding element in the input label
-      to the PASCAL color map.
+	Returns:
+		result: A 2D array with floating type. The element of the array
+			is the color indexed by the corresponding element in the input label
+			to the PASCAL color map.
 
-  Raises:
-    ValueError: If label is not of rank 2 or its value is larger than color
-      map maximum entry.
-  """
-  if label.ndim != 2:
-    raise ValueError('Expect 2-D input label')
+	Raises:
+		ValueError: If label is not of rank 2 or its value is larger than color
+			map maximum entry.
+	"""
+	if label.ndim != 2:
+		raise ValueError('Expect 2-D input label')
 
-  colormap = create_pascal_label_colormap()
+	colormap = create_pascal_label_colormap()
 
-  if np.max(label) >= len(colormap):
-    raise ValueError('label value too large.')
+	if np.max(label) >= len(colormap):
+		raise ValueError('label value too large.')
 
-  return colormap[label]
+	return colormap[label]
 
 
 def main():
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--model', required=True,
-                      help='Path of the segmentation model.')
-  parser.add_argument('--input', required=True,
-                      help='File path of the input image.')
-  parser.add_argument('--output', default='semantic_segmentation_result.jpg',
-                      help='File path of the output image.')
-  parser.add_argument(
-      '--keep_aspect_ratio',
-      action='store_true',
-      default=False,
-      help=(
-          'keep the image aspect ratio when down-sampling the image by adding '
-          'black pixel padding (zeros) on bottom or right. '
-          'By default the image is resized and reshaped without cropping. This '
-          'option should be the same as what is applied on input images during '
-          'model training. Otherwise the accuracy may be affected and the '
-          'bounding box of detection result may be stretched.'))
-  args = parser.parse_args()
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--model', required=True,
+				help='Path of the segmentation model.')
+	parser.add_argument('--output', default='semantic_segmentation_result.jpg',
+				help='File path of the output image.')
+	parser.add_argument(
+			'--keep_aspect_ratio',
+			action='store_true',
+			default=False,
+			help=(
+				'keep the image aspect ratio when down-sampling the image by adding '
+				'black pixel padding (zeros) on bottom or right. '
+				'By default the image is resized and reshaped without cropping. This '
+				'option should be the same as what is applied on input images during '
+				'model training. Otherwise the accuracy may be affected and the '
+				'bounding box of detection result may be stretched.'))
+	args = parser.parse_args()
 
-  imageArray = myPylonImage()
-  # myPylonImage()
+	cam = BaslerCam()
+	imageArray = cam.grabImage()
 
-  interpreter = make_interpreter(args.model, device=':0')
-  interpreter.allocate_tensors()
-  width, height = common.input_size(interpreter)
- 
-  # img = Image.open("saved_pypylon_img.bmp")
-  img = imageArray
+	interpreter = make_interpreter(args.model, device=':0')
+	interpreter.allocate_tensors()
+	width, height = common.input_size(interpreter)
 
-  print("img.shape()", img.shape)
-  #  sys.exit()
+	#img = Image.open("saved_pypylon_img.bmp")
+	img = imageArray
+
+	print("img.shape()", img.shape)
+	#sys.exit()
 
 
-  if args.keep_aspect_ratio:
-    resized_img, _ = common.set_resized_input(
-        interpreter, img.size, lambda size: img.resize(size, Image.ANTIALIAS))
-  else:
-    # resized_img = img.resize((width, height), Image.ANTIALIAS)
-    resized_img = cv2.resize(img, (width, height), interpolation = cv2.INTER_AREA)
-    common.set_input(interpreter, resized_img)
+	if args.keep_aspect_ratio:
+		resized_img, _ = common.set_resized_input(
+				interpreter, img.size, lambda size: img.resize(size, Image.ANTIALIAS))
+	else:
+		# resized_img = img.resize((width, height), Image.ANTIALIAS)
+		resized_img = cv2.resize(img, (width, height), interpolation = cv2.INTER_AREA)
+		common.set_input(interpreter, resized_img)
 
 
-  interpreter.invoke()
+	interpreter.invoke()
 
-  result = segment.get_output(interpreter)
-  if len(result.shape) == 3:
-    result = np.argmax(result, axis=-1)
+	result = segment.get_output(interpreter)
+	if len(result.shape) == 3:
+		result = np.argmax(result, axis=-1)
 
-  # If keep_aspect_ratio, we need to remove the padding area.
-  new_height, new_width, new_channel = resized_img.shape
-  print("resized_img.shape",resized_img.shape)
+	# If keep_aspect_ratio, we need to remove the padding area.
+	new_height, new_width, new_channel = resized_img.shape
+	print("resized_img.shape",resized_img.shape)
 
-# new_width, new_height, new_channel = resized_img.size
-  result = result[:new_height, :new_width]
-  mask_img = Image.fromarray(label_to_color_image(result).astype(np.uint8))
+	# new_width, new_height, new_channel = resized_img.size
+	result = result[:new_height, :new_width]
+	mask_img = Image.fromarray(label_to_color_image(result).astype(np.uint8))
 
-  # Concat resized input image and processed segmentation results.
-  output_img = Image.new('RGB', (2 * new_width, new_height))
-  output_img.paste(resized_img, (0, 0))
-  output_img.paste(mask_img, (width, 0))
-  output_img.save(args.output)
-  print('Done. Results saved at', args.output)
+	# Concat resized input image and processed segmentation results.
+	output_img = Image.new('RGB', (2 * new_width, new_height))
+	output_img.paste(resized_img, (0, 0))
+	output_img.paste(mask_img, (width, 0))
+	output_img.save(args.output)
+	print('Done. Results saved at', args.output)
 
 if __name__ == '__main__':
-  main()
+	main()
