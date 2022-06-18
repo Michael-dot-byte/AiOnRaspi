9f49a5b89c82056b28a8c825b1483d158519bf07
lord-of-death <tim.steinbach93@gmail.com>
2022-06-18 17:36:21 +0200

    updated netOnCoral.py for 'tfKerasChars10Epochs' net

--- kerasCharRecognition/netOnCoral.py
+++ kerasCharRecognition/netOnCoral.py
@@ -1,32 +1,3 @@
-# Lint as: python3
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#		 https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-r"""An example of semantic segmentation.
-
-The following command runs this script and saves a new image showing the
-segmented pixels at the location specified by `output`:
-
-```
-bash examples/install_requirements.sh semantic_segmentation.py
-
-python3 examples/semantic_segmentation.py \
-	--model test_data/deeplabv3_mnv2_pascal_quant_edgetpu.tflite \
-	--keep_aspect_ratio \
-	--output ${HOME}/segmentation_result.jpg
-```
-"""
-
 import argparse
 import numpy as np
 import sys
