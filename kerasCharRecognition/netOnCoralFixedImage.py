a623771e1c74c2d6b4460c2bff846525d142dc37
lord-of-death <tim.steinbach93@gmail.com>
2022-07-24 12:07:45 +0200

    updated netOnCoralFixedImage.py to classify all letters of all words in a single call of the script; remaining bug: letters are not classified correctly

--- kerasCharRecognition/netOnCoralFixedImage.py
+++ kerasCharRecognition/netOnCoralFixedImage.py
@@ -57,7 +57,9 @@ class BaslerCam():
 
 def main():
 
-	charList = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+	charList = ['0','1','2','3','4','5','6','7','8','9',
+	'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+	'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 	#rudimentäre Ausschriften vom Coral einschalten
 	pycoral.utils.edgetpu.set_verbosity(1)
@@ -80,10 +82,13 @@ def main():
 	#Festes Bild laden und vorverarbeiten
 	image  = cv2.imread("testBild.jpg")
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	thresh = cv2.adaptiveThreshold(gray, 255, 1, 1, 61, 20)
+	threshInv = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 61, 20)
+
+#	plt.imsave("gray.png", gray, cmap='gray')
+#	plt.imsave("threshInv.png", threshInv, cmap='gray')
 
 	#Rechtecke ermitteln
-	contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	contours, _ = cv2.findContours(threshInv, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 	cnt = contours[0]
 
 	rects = []
@@ -95,7 +100,8 @@ def main():
 	words = []
 	letters = []
 
-	inverted = cv2.adaptiveThreshold(gray, 255, 1, 1, 61, 20)
+	thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 61, 20)
+#	plt.imsave("thresh.png", thresh, cmap='gray')
 
 	alt_y = -1
 
@@ -107,53 +113,74 @@ def main():
 				words.append(letters)
 				letters = []
 
-			#get single letter from big image
-			crop_img = inverted[y:y+h, x:x+w]
+			if False: #uninverted image
+				#get single letter from big image
+				crop_img = thesh[y:y+h, x:x+w]
 
-			#only take letters which are not blank
-			if crop_img.sum() > 56610:
-				letters.append(crop_img)
-				alt_y = y
+				#only take letters which are not blank
+				if crop_img.sum() < 753032:
+					letters.append(crop_img)
+					alt_y = y
+			else: #inverted image
+				#get single letter from big image
+				crop_img = threshInv[y:y+h, x:x+w]
 
+				#only take letters which are not blank
+				if crop_img.sum() > 56610:
+					letters.append(crop_img)
+					alt_y = y
+
+	#append last Word, as there is no linebreak
 	words.append(letters)
 
-	imageArray = words[0][0]
+	#interate trough all letters of all words to the separated chars
+	for word in words:
+		str = ''
+		cnt = 0
+		for l in word:
+			imageArray = l
 
-	#Bild von Eingangsgöße auf 28x28 skalieren
-	resImage = cv2.resize(imageArray, dsize=(28,28), interpolation=cv2.INTER_CUBIC)
-	#Daten von uint8 auf fp16 ändern und auf Werte von 0 bis 1 bringen
-	normImage = resImage.astype(np.float32)[:]/255
-	#print(type(normImage[1,1]))
-	#print(normImage)
-	#img.imsave("test.png", resImage)
+			#Bild von Eingangsgöße auf 28x28 skalieren
+			resImage = cv2.resize(imageArray, dsize=(28,28), interpolation=cv2.INTER_CUBIC)
+			#Daten von uint8 auf fp32 ändern und auf Werte von 0 bis 1 bringen
+			normImage = resImage.astype(np.float32)[:]/255
+			#print(type(normImage[1,1]))
+			#print(normImage)
+	#		plt.imsave("{0}.png".format(cnt), normImage, cmap='gray')
+	#		cnt += 1
 
-	print("Form der Input Daten", normImage.shape)
+	#		print("Form der Input Daten", normImage.shape)
 
-	#Input für das Netz setzen
-	common.set_input(interpreter, normImage)
-	#Unterschiedliche Infos zum Input Tensor erfassen
-	#print("Daten des Input Tensors des Netzes", common.input_tensor(interpreter))
-	#print("Input Details", common.input_details(interpreter, "shape"))
-	interpreter_infos = interpreter.get_input_details()[0]
-	for _ in interpreter_infos:
-		print(_,  interpreter_infos[_])
+			#Input für das Netz setzen
+			common.set_input(interpreter, normImage)
+			#Unterschiedliche Infos zum Input Tensor erfassen
+			#print("Daten des Input Tensors des Netzes", common.input_tensor(interpreter))
+			#print("Input Details", common.input_details(interpreter, "shape"))
+			interpreter_infos = interpreter.get_input_details()[0]
+	#		for _ in interpreter_infos:
+	#			print(_,  interpreter_infos[_])
 
-	#NN laufen lassen
-	startZeit= time.process_time_ns()
-	interpreter.invoke()
-	print("Klassifizierungszeit in µs {0}".format((time.process_time_ns() - startZeit)/1000))
+			#NN laufen lassen
+			startZeit= time.process_time_ns()
+	#		pycoral.utils.edgetpu.run_inference(interpreter, normImage)
+			interpreter.invoke()
+	#		print("Klassifizierungszeit in µs {0}".format((time.process_time_ns() - startZeit)/1000))
 
-	#Ergebnis vom NN abholen
-	output = common.output_tensor(interpreter, 0)
-	#print("Output Tensor", output)
+			#Ergebnis vom NN abholen
+			output = common.output_tensor(interpreter, 0)
+			#print("Output Tensor", output)
 
-	#Ergebnis aufbereiten
-	print("Zeichen unmapped", np.argmax(output[0]))
-	print("Zeichen mapped", charList[np.argmax(output[0])])
-	print("Wahrscheinlichkeit", np.max(output[0]))
-	print("Summe", np.sum(output[0]))
-	print(output[0][:10])
-	print("Länge Outputs", output.shape)
+			#Ergebnis aufbereiten
+	#		print("Zeichen unmapped", np.argmax(output[0]))
+	#		print("Zeichen mapped", charList[np.argmax(output[0])])
+	#		print("Wahrscheinlichkeit", np.max(output[0]))
+			str += charList[np.argmax(output[0])]
+
+			del output
+	#		print("Summe", np.sum(output[0]))
+	#		print(output[0][:10])
+	#		print("Länge Outputs", output.shape)
+		print(str)
 
 if __name__ == '__main__':
 	main()
